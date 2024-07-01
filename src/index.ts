53cfb35ccdd24859751e1f25ee6b1cd9d76c35aa
landonwjohnson <landonwjohnson@gmail.com>
2024-07-01 12:14:14 -0600

    changes hav ebeen made

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,154 @@ export * from "./findItemByLetterAndFilter";
 /**
  * TSDoc for isJson
  */
+<<<<<<< Updated upstream
 export * from "./isJson";
+=======
+export * from "./isJson";
+/**
+<<<<<<< Updated upstream
+ * TSDoc for isNumber
+ */
+export * from "./isNumber";
+/**
+ * TSDoc for isPlainObject
+ */
+export * from "./isPlainObject";
+/**
+ * TSDoc for isRegExp
+ */
+export * from "./isRegExp";
+/**
+ * TSDoc for isString
+ */
+export * from "./isString";
+/**
+ * TSDoc for isUndefined
+ */
+export * from "./isUndefined";
+/**
+ * TSDoc for isValidHex
+ */
+export * from "./isValidHex";
+/**
+ * TSDoc for isValidStyle
+ */
+export * from "./isValidStyle";
+/**
+=======
+>>>>>>> Stashed changes
+ * TSDoc for jsonContainsNewLine
+ */
+export * from "./jsonContainsNewLine";
+/**
+<<<<<<< Updated upstream
+ * TSDoc for kindOf
+ */
+export * from "./kindOf";
+/**
+ * TSDoc for kindOfTest
+ */
+export * from "./kindOfTest";
+/**
+ * TSDoc for applyFormatters
+ */
+export * from "./logFormattedPhrases/applyFormatters";
+/**
+ * TSDoc for formatPhrase
+ */
+export * from "./logFormattedPhrases/formatPhrase";
+/**
+ * TSDoc for getPhraseBackground
+ */
+export * from "./logFormattedPhrases/getPhraseBackground";
+/**
+ * TSDoc for getPhraseStylesFromOrder
+ */
+export * from "./logFormattedPhrases/getPhraseStylesFromOrder";
+/**
+ * TSDoc for logFormattedPhrases
+ */
+export * from "./logFormattedPhrases/logFormattedPhrases";
+/**
+ * TSDoc for logPhrases
+ */
+export * from "./logFormattedPhrases/logPhrases";
+/**
+ * TSDoc for matchAll
+ */
+export * from "./matchAll";
+/**
+ * TSDoc for merge
+ */
+export * from "./merge";
+/**
+ * TSDoc for mergeObjects
+ */
+export * from "./mergeObjects";
+/**
+ * TSDoc for parseHeaders
+ */
+export * from "./parseHeaders";
+/**
+ * TSDoc for parsePropPath
+ */
+export * from "./parsePropPath";
+/**
+ * TSDoc for pluralize
+ */
+export * from "./pluralize";
+/**
+ * TSDoc for reduceDescriptors
+ */
+export * from "./reduceDescriptors";
+/**
+=======
+>>>>>>> Stashed changes
+ * TSDoc for removeNewLinesFromJson
+ */
+export * from "./removeNewLinesFromJson";
+/**
+ * TSDoc for removeNewLinesFromString
+ */
+export * from "./removeNewLinesFromString";
+/**
+ * TSDoc for removeSpacesFromJson
+ */
+export * from "./removeSpacesFromJson";
+/**
+ * TSDoc for removeSpacesFromString
+ */
+<<<<<<< Updated upstream
+export * from "./removeSpacesFromString";
+/**
+ * TSDoc for rgbaToHexAlpha
+ */
+export * from "./rgbaToHexAlpha";
+/**
+ * TSDoc for safeCssProperties
+ */
+export * from "./safeCssProperties";
+/**
+ * TSDoc for toCamelCase
+ */
+export * from "./toCamelCase";
+/**
+ * TSDoc for toFlatObject
+ */
+export * from "./toFlatObject";
+/**
+ * TSDoc for toObjectSet
+ */
+export * from "./toObjectSet";
+/**
+ * TSDoc for typeToTest
+ */
+export * from "./typeToTest";
+/**
+ * TSDoc for waitFor
+ */
+export * from "./waitFor";
+=======
+export * from "./removeSpacesFromString";
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
--- typings/isJson.d.ts
+++ typings/isJson.d.ts
@@ -3,4 +3,12 @@
  * @param str The string to check.
  * @returns Returns the parsed JSON object if the string is valid JSON, otherwise returns false.
  */
+<<<<<<< Updated upstream
 export declare const isJson: (value: string | object) => JSON | boolean;
+=======
+export declare const isJson: (value: string | object) => Record<string, unknown> | boolean;
+<<<<<<< Updated upstream
+export declare const isObject: (thing: unknown) => boolean;
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
